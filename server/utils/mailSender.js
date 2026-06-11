d073e867b5d796ea970a0e0aba2ac178d0e26235
prashantk-coder <prashantkchahar@gmail.com>
2026-06-11 14:12:06 +0530

    add brevo smtp debug

--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -2,25 +2,15 @@ const nodemailer = require("nodemailer");
 
 const mailSender = async (email, title, body) => {
   try {
-    console.log("=================================");
-    console.log("MAIL_HOST:", process.env.MAIL_HOST);
-    console.log("MAIL_PORT:", process.env.MAIL_PORT);
-    console.log("MAIL_USER:", process.env.MAIL_USER);
-    console.log("SENDING EMAIL TO:", email);
-    console.log("=================================");
-
     const transporter = nodemailer.createTransport({
       host: process.env.MAIL_HOST,
       port: Number(process.env.MAIL_PORT),
       secure: false,
-
       auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASS,
       },
     });
-
-    console.log("TRANSPORTER CREATED SUCCESSFULLY");
 
     const info = await transporter.sendMail({
       from: `"StudySphere" <prashantkchahar@gmail.com>`,
@@ -29,16 +19,11 @@ const mailSender = async (email, title, body) => {
       html: body,
     });
 
-    console.log("EMAIL SENT SUCCESSFULLY");
-    console.log("MESSAGE ID:", info.messageId);
-    console.log("RESPONSE:", info.response);
+    console.log("Email Sent:", info.messageId);
 
     return info;
   } catch (error) {
-    console.log("=================================");
-    console.log("MAIL ERROR =>", error);
-    console.log("=================================");
-
+    console.error("MAIL ERROR:", error.message);
     throw error;
   }
 };
